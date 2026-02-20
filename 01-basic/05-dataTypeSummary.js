51c5e010841fa8ad13f8d4ecf1582b25ad1b5d34
Mubashir Hasan <89275827+Mushi105@users.noreply.github.com>
2026-02-20 06:25:06 +0000

    update

--- 01-basic/05-dataTypeSummary.js
+++ 01-basic/05-dataTypeSummary.js
@@ -10,3 +10,53 @@ const scoreValue = 100.3
 
 const isLoggedIn = false
 const outsideTyemp = null
+
+
+console.log("Mubashir Hasan");
+
+
+const accountId=243242342
+let name='Mubashir Hasan'
+var city='Karachi'
+datatype='test'
+let variable;
+var cityvar;
+//console.table([accountId,name,city,datatype,variable,cityvar])
+
+/*
+
+Prefer not to use var
+becasue of issue in block scope and funation  scope
+
+*/
+
+
+
+// Primititve
+// 7 type String, Number, Boolean, Null, Undefince, Symbol
+//BigInt
+// Array Object Function
+
+
+
+const id = Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id);
+const bigNumber = 12323131n
+console.log(bigNumber);
+
+
+
+const heros = ["Imam Zaman", "Hussain", "Ali"];
+let myObj=
+{
+    name:"Mubashir",
+    age:22,
+
+
+}
+
+
+console.log(myObj);
+
