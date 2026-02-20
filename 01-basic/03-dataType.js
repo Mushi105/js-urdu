d8a348cf665eed176c3ca41d40c72889928fc90d
Mubashir Hasan <89275827+Mushi105@users.noreply.github.com>
2026-02-20 06:01:13 +0000

    upadte ting data type

--- 01-basic/03-dataType.js
+++ 01-basic/03-dataType.js
@@ -8,6 +8,39 @@ let score="true"
 
 let valueofBoolen=""
 
- console.log(typeof valueofBoolen);
- let convertBoolNumber=Boolean (valueofBoolen)
- console.log(convertBoolNumber);
+//  console.log(typeof valueofBoolen);
+//  let convertBoolNumber=Boolean (valueofBoolen)
+//  console.log(convertBoolNumber);
+
+
+
+ /************ Operation Perfom *************/
+
+
+ let value=3
+ let negValue=-value
+// console.log(negValue);
+ 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+// let st1 = "Mubashir"
+// let st2 = " Hasan"
+// console.log(st1+st2);
+
+
+//console.log("1"+2+4 +"5");
+
+// let num1, num2, num3
+//  num1 = num2 =num3=2+3
+
+//  console.log(num1);
+ 
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
