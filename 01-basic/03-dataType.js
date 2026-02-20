24ba444233460e8f79fc2f4b23e6db05a939dff1
Mubashir Hasan <89275827+Mushi105@users.noreply.github.com>
2026-02-20 05:27:52 +0000

    datatype learing

---
+++ 01-basic/03-dataType.js
@@ -0,0 +1,13 @@
+let score="true"
+
+//console.log(typeof score);
+// let valueNumber=Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+let valueofBoolen=""
+
+ console.log(typeof valueofBoolen);
+ let convertBoolNumber=Boolean (valueofBoolen)
+ console.log(convertBoolNumber);
