ce1cdbbfbfd67a260663b9a0bd37631b5eb3da12
Mubashir Hasan <89275827+Mushi105@users.noreply.github.com>
2026-02-24 04:11:58 +0000

    update

---
+++ 03-basic/05-function.js
@@ -0,0 +1,21 @@
+// function code(){
+//     console.log('Hello DB Connected');
+    
+// }
+
+
+(function chia(){
+    console.log('Hello Db Connected');
+    
+})();
+
+(function code(){
+    console.log('Hello DB Connected Two');
+    
+})();
+//code();
+
+((name)=>{
+    console.log(`DB Conncted 3 ${name}`);
+    
+})('Mubashir DB Cdata')
